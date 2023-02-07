2a08d8fdb9b90142119873bc9120a8c074a50f8b
Oluwadare Emmanuel <oluwadare.emmanuel15@gmail.com>
2023-02-07 17:56:17 +0100

    typeIT

--- app/src/main/java/com/example/typeit/MainActivity.java
+++ app/src/main/java/com/example/typeit/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.typeit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button StartBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/typeit/TestActivity.java
@@ -0,0 +1,79 @@
+package com.example.typeit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class TestActivity extends AppCompatActivity {
+    TextView DisplayedWords, GameResult;
+    EditText EnteredWords;
+    Button ResetBtn;
+    String fullStory;
+    long BeginTime, StopTime;
+    boolean gameBegan = false;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        DisplayedWords = (TextView) findViewById(R.id.DisplayedWords);
+        GameResult = (TextView) findViewById(R.id.GameResult);
+        EnteredWords = (EditText) findViewById(R.id.EnteredWords);
+        ResetBtn = (Button) findViewById(R.id.button);
+        fullStory = DisplayedWords.getText().toString();
+
+        EnteredWords.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String currentStory = EnteredWords.getText().toString();
+
+                //startedTask
+                if(currentStory.length() ==1 && ! gameBegan ){
+                    BeginTime = System.currentTimeMillis();
+                    GameResult.setText("Test began");
+                    gameBegan = true;
+                }
+                //EndedTask
+                if(currentStory.equals(fullStory)){
+                    StopTime = System.currentTimeMillis();
+
+                    long currentTime = (StopTime - BeginTime) / 1000;
+                    GameResult.setText("Finished in " + currentTime + " seconds");
+
+                    EnteredWords.setEnabled(false);
+                    EnteredWords.clearFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        ResetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EnteredWords.setEnabled(true);
+                EnteredWords.setText("");
+                GameResult.setText("");
+                gameBegan = false;
+            }
+        });
+
+    }
+}
