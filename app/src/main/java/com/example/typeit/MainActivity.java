e81bf773df388225eb54ceb0583757becf358343
Oluwadare Emmanuel <oluwadare.emmanuel15@gmail.com>
2023-02-08 22:26:02 +0100

    TypeIT

--- app/src/main/java/com/example/typeit/MainActivity.java
+++ app/src/main/java/com/example/typeit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.typeit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -12,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StartBtn = findViewById(R.id.StartBtn);
+
+        Intent intent = new Intent(MainActivity.this, TestActivity.class);
     }
 }
