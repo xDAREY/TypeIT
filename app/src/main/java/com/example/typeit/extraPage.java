8edef2c5ad4cd751f01dec57c4807b5fa325831a
Oluwadare Emmanuel <oluwadare.emmanuel15@gmail.com>
2023-02-08 22:49:10 +0100

    done

---
+++ app/src/main/java/com/example/typeit/extraPage.java
@@ -0,0 +1,14 @@
+package com.example.typeit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class extraPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extra_page);
+    }
+}
