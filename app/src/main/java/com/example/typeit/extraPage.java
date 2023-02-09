3588b4939a75d966ca90afff060aa0aa5e69d535
Oluwadare Emmanuel <oluwadare.emmanuel15@gmail.com>
2023-02-09 23:00:00 +0100

    done

--- app/src/main/java/com/example/typeit/extraPage.java
+++ app/src/main/java/com/example/typeit/extraPage.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class extraPage extends AppCompatActivity {
 
@@ -12,5 +16,11 @@ public class extraPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extra_page);
+
+        Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+        homeIntent.addCategory(Intent.CATEGORY_HOME);
+        homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
     }
 }
+
