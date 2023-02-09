2a1a3c7d764586bc1f5ab7a6c9a4c9fbce481359
Oluwadare Emmanuel <oluwadare.emmanuel15@gmail.com>
2023-02-09 23:21:08 +0100

    done

--- app/src/main/java/com/example/typeit/extraPage.java
+++ app/src/main/java/com/example/typeit/extraPage.java
@@ -11,11 +11,21 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class extraPage extends AppCompatActivity {
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extra_page);
+
+        back = findViewById(R.id.backbtn)
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(extraPage.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Intent homeIntent = new Intent(Intent.ACTION_MAIN);
         homeIntent.addCategory(Intent.CATEGORY_HOME);
